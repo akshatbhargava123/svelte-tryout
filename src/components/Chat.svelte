65b02a981a5fc08cd90086d0fe16ed8a30e1e239
akshat <akshatbhargavadeepa@gmail.com>
2021-10-11 19:44:43 +0530

    feat: chat component now stores messages properly

--- src/components/Chat.svelte
+++ src/components/Chat.svelte
@@ -1,19 +1,46 @@
 <script>
   import { onMount } from "svelte";
+  import { getFirestore, getDocs, addDoc, collection } from 'firebase/firestore';
+  import { getAuth } from 'firebase/auth';
+  import Message from './Message.svelte';
 
+  let inputText = '';
   let loading = false;
   let chatMessages = [];
-  
-  export let userName;
 
+  const firestore = getFirestore();
+  const auth = getAuth();
+  
+  function createMessage(text, timestamp = Date.now()) {
+    return {
+      text,
+      timestamp,
+      userId: auth.currentUser.uid,
+    };
+  }
+
+  async function onSend() {
+    const messagePayload = createMessage(inputText);
+    await addDoc(collection(firestore, '/chats'), messagePayload);
+
+    inputText = '';
+  }
+  
   onMount(() => {
     
   });
+
+  export let userName;
 </script>
 
 <div class="chat-container">
   
-  <input class="chat-input" type="text" placeholder={`Send a message as ${userName}`}>
+  {#each chatMessages as message, idx (idx)}
+    <Message message={message} />
+  {/each}
+
+  <input class="chat-input" type="text" bind:value={inputText} placeholder={`Send a message as ${userName}`}>
+  <button on:click={onSend}>Submit</button>
 </div>
 
 <style>
