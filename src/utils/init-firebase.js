a6e0b91e54fd9d412759a5d69cac5f0a270b296c
akshat <akshatbhargavadeepa@gmail.com>
2021-10-11 19:15:31 +0530

    chore: setup firebase

---
+++ src/utils/init-firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import 'firebase/auth';
+import 'firebase/firestore';
+
+// app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBWjPP4cvC34t1FZW8ZjH1nxwFlqcS4BuM",
+  authDomain: "chat-app-5cddb.firebaseapp.com",
+  projectId: "chat-app-5cddb",
+  storageBucket: "chat-app-5cddb.appspot.com",
+  messagingSenderId: "605623232266",
+  appId: "1:605623232266:web:ef416ce8d47ece750dfcf3"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
