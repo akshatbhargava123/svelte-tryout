1a5b2809249979f2b35ae6584d030c4266485c4f
akshat <akshatbhargavadeepa@gmail.com>
2021-10-11 19:56:01 +0530

    feat: created message component

---
+++ src/components/Message.svelte
@@ -0,0 +1,35 @@
+<script>
+  import format from 'date-fns/format';
+  export let message;
+</script>
+
+<div class="message-container">
+  <p class="text">{message.text}</p>
+  <span class="timestamp">{format(message.timestamp, 'HH:mm aaa')}</span>
+  <span class="user">~{message.userName || ''}</span>
+</div>
+
+<style>
+  .message-container {
+    border: 1px solid lightgrey;
+    padding: 5px 10px;
+    display: flex;
+    align-items: center;
+  }
+
+  .text {
+    font-size: 1.25rem;
+    margin-right: 10px;
+  }
+
+  .timestamp {
+    color: gray;
+    font-size: 0.75rem;
+    margin-right: 4px;
+  }
+
+  .user {
+    color: gray;
+    font-size: 0.75rem;
+  }
+</style>
