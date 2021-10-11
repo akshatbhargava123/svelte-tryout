860f42f25acbc4a113facb756d08249825987c5b
akshat <akshatbhargavadeepa@gmail.com>
2021-10-11 19:32:21 +0530

    feat: created chat component

--- src/components/Chat.svelte
+++ src/components/Chat.svelte
@@ -1,9 +1,32 @@
 <script>
+  import { onMount } from "svelte";
+
   let loading = false;
+  let chatMessages = [];
   
   export let userName;
+
+  onMount(() => {
+    
+  });
 </script>
 
-<div>
-  Welcome to chat {userName}
+<div class="chat-container">
+  
+  <input class="chat-input" type="text" placeholder={`Send a message as ${userName}`}>
 </div>
+
+<style>
+  .chat-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 50vw;
+  }
+
+  .chat-input {
+    border-radius: 4px;
+    padding: 8px 6px;
+    width: 70%;
+  }
+</style>
